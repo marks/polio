ed4cc854aa86dbca2b3e5d6c6b02b21cded84264
Bret Fontecchio <bret@seedscientific.com>
2015-04-28 17:42:40 -0400

    Set Up Testing (Skeleton Code)

---
+++ entity/tests/test_api.py
@@ -0,0 +1,16 @@
+from tastypie.test import ResourceTestCase
+
+from django.conf import settings
+
+from django.contrib.auth.models import User, Group
+
+class CreateUserTest(ResourceTestCase):
+
+    def setUp(self):
+        pass
+    def setup_session(self):
+        pass
+    def test_unauthorized(self):
+        pass
+    def test_next_entry(self):
+        pass
